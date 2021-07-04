9a898ed397368ee0bf8366923f6d2a807e8dc097
cqh963852 <cqh963852@hotmail.com>
2021-07-04 22:43:01 +0800

    test: update test case

--- packages/sns/test/slot.test.tsx
+++ packages/sns/test/slot.test.tsx
@@ -3,16 +3,19 @@ import { FC, StrictMode, useCallback, useState } from "react";
 import SnsProvider, { useSetSlot, useSlot, useSns } from "src/SnsProvider";
 
 describe("useSlot useSetSlot StrictMode", () => {
-    test('should has one slot', () => {
+    test("should has one slot", () => {
         const wrapper: FC = (props) => {
             const { children } = props;
-            return <StrictMode><SnsProvider>{children}</SnsProvider></StrictMode>;
+            return (
+                <StrictMode>
+                    <SnsProvider>{children}</SnsProvider>
+                </StrictMode>
+            );
         };
         const { result } = renderHook(
             () => {
                 const [testId] = useState(Symbol("A"));
-                useSetSlot(testId)
-                const slot = useSlot(testId);
+                const slot = useSetSlot(testId);
                 const sns = useSns();
                 return { slot, sns };
             },
@@ -22,18 +25,52 @@ describe("useSlot useSetSlot StrictMode", () => {
         );
         // @ts-ignore
         expect(result.current.sns.slots.length).toBe(1);
-    })
+    });
 
-    test('should has one slot if id is same', () => {
+    test("should has one slot after changeTestId", () => {
         const wrapper: FC = (props) => {
             const { children } = props;
-            return <StrictMode><SnsProvider>{children}</SnsProvider></StrictMode>;
+            return (
+                <StrictMode>
+                    <SnsProvider>{children}</SnsProvider>
+                </StrictMode>
+            );
+        };
+        const { result } = renderHook(
+            () => {
+                const [testId, setTestId] = useState(Symbol("A"));
+                const slot = useSetSlot(testId);
+                const sns = useSns();
+                const changeTestId = useCallback(() => {
+                    setTestId(Symbol("B"));
+                }, []);
+
+                return { slot, sns, changeTestId };
+            },
+            {
+                wrapper,
+            }
+        );
+        act(() => {
+            result.current.changeTestId();
+        });
+        // @ts-ignore
+        expect(result.current.sns.slots.length).toBe(1);
+    });
+
+    test("should has one slot if id is same", () => {
+        const wrapper: FC = (props) => {
+            const { children } = props;
+            return (
+                <StrictMode>
+                    <SnsProvider>{children}</SnsProvider>
+                </StrictMode>
+            );
         };
         const { result } = renderHook(
             () => {
                 const [testId] = useState("A");
-                useSetSlot(testId)
-                const slot = useSlot(testId);
+                const slot = useSetSlot(testId);
                 const sns = useSns();
                 return { slot, sns };
             },
@@ -59,56 +96,9 @@ describe("useSlot useSetSlot StrictMode", () => {
         // @ts-ignore
         expect(result2.current.sns.slots.length).toBe(1);
         // @ts-ignore
-        expect(result.current.sns.slots[0]).toEqual(result2.current.sns.slots[0])
-    })
-
-    test('should has one slot after changeTestId', () => {
-        const wrapper: FC = (props) => {
-            const { children } = props;
-            return <StrictMode><SnsProvider>{children}</SnsProvider></StrictMode>;
-        };
-        const { result } = renderHook(
-            () => {
-                const [testId, setTestId] = useState(Symbol("A"));
-                const slot = useSlot(testId);
-                useSetSlot(testId)
-                const sns = useSns();
-                const changeTestId = useCallback(() => {
-                    setTestId(Symbol("B"));
-                }, []);
-
-                return { slot, sns, changeTestId };
-            },
-            {
-                wrapper,
-            }
+        expect(result.current.sns.slots[0]).toEqual(
+            // @ts-ignore
+            result2.current.sns.slots[0]
         );
-        act(() => {
-            result.current.changeTestId();
-        });
-        // @ts-ignore
-        expect(result.current.sns.slots.length).toBe(1);
-    })
-
-
-
-    // const { result } = renderHook(
-    //     () => {
-    //         const [testId, setTestId] = useState(Symbol("A"));
-    //         const slot = useSlot(testId);
-
-    //         const sns = useSns();
-    //         const changeTestId = useCallback(() => {
-    //             setTestId(symbolB);
-    //         }, []);
-    //         console.log(sns)
-
-    //         return { slot, sns, changeTestId };
-    //     },
-    //     {
-    //         wrapper,
-    //     }
-    // );
-
-
+    });
 });
