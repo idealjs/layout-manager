a8d34c39dff95bab1d254a148475a2e27b2a52f2
cqh963852 <cqh963852@hotmail.com>
2022-04-05 19:46:57 +0800

    feat: add PortalWindow

--- packages/grape-layout/src/components/Popout.tsx
+++ packages/grape-layout/src/components/Popout.tsx
@@ -4,21 +4,23 @@ import {
     LayoutNode,
     Provider,
 } from "@idealjs/layout-manager";
+import PortalWindow from "@idealjs/portal-window";
 import { FC, useCallback, useRef } from "react";
 import { useMemo } from "react";
 
 import CustomSplitter from "./CustomSplitter";
 import CustomTab from "./CustomTab";
+import { sheet as customTabSheet } from "./CustomTab";
 import CustomTitlebar from "./CustomTitlebar";
+import { sheet as customTitlebarSheet } from "./CustomTitlebar";
 import { useFactory } from "./FactoryProvider";
 import layoutJSON from "./layout.json";
 import { usePortals } from "./PopoutManager";
-import Portal from "./Portal";
 
 const Popout: FC<{ portalId: string | number }> = (props) => {
     const { portalId } = props;
     const portalRef = useRef<{ close: () => void }>(null);
-    const { portalsRef } = usePortals();
+    const { portalsRef, setPortals } = usePortals();
 
     const ROOT = useMemo(
         () =>
@@ -45,13 +47,35 @@ const Popout: FC<{ portalId: string | number }> = (props) => {
         },
         [portalsRef]
     );
+
     const updateHook = useMemo(
         () => ({
             after: afterUpdate,
         }),
         [afterUpdate]
     );
+
     const factory = useFactory();
+
+    const onExtBeforeUnload = useCallback(() => {
+        setPortals((state) => state.filter((d) => d !== portalId));
+    }, [portalId, setPortals]);
+
+    const onMainBeforeunload = useCallback(() => {
+        portalRef.current?.close();
+    }, []);
+
+    const injectCSS = useCallback((extWindow: Window) => {
+        console.log("test test injectCSS", extWindow);
+        const style = extWindow.document.head.appendChild(
+            extWindow.document.createElement("style")
+        );
+        style.textContent = "".concat(
+            customTitlebarSheet.toString(),
+            customTabSheet.toString()
+        );
+    }, []);
+
     return (
         <Provider
             layoutNode={ROOT}
@@ -64,9 +88,14 @@ const Popout: FC<{ portalId: string | number }> = (props) => {
             splitterThickness={5}
             titlebarHeight={30}
         >
-            <Portal ref={portalRef} id={portalId}>
+            <PortalWindow
+                ref={portalRef}
+                onExtBeforeUnload={onExtBeforeUnload}
+                onMainBeforeUnload={onMainBeforeunload}
+                afterHook={injectCSS}
+            >
                 <Layout />
-            </Portal>
+            </PortalWindow>
         </Provider>
     );
 };
---
+++ packages/portal-window/src/PortalWindow.tsx
@@ -0,0 +1,82 @@
+import {
+    forwardRef,
+    ReactNode,
+    useEffect,
+    useImperativeHandle,
+    useRef,
+} from "react";
+import { createPortal } from "react-dom";
+
+interface IProps {
+    children?: ReactNode;
+    onExtBeforeUnload?: () => void;
+    onMainBeforeUnload?: () => void;
+    afterHook?: (extWindow: Window) => void;
+}
+
+const PortalWindow = forwardRef<{}, IProps>((props, ref) => {
+    const { children, onExtBeforeUnload, onMainBeforeUnload, afterHook } =
+        props;
+
+    const containerRef = useRef<HTMLDivElement>(document.createElement("div"));
+    const externalWindowRef = useRef<Window | null>();
+
+    useImperativeHandle(
+        ref,
+        () => ({
+            close: () => {
+                externalWindowRef.current?.close();
+            },
+        }),
+        []
+    );
+
+    useEffect(() => {
+        if (externalWindowRef.current == null) {
+            externalWindowRef.current = window.open(
+                "",
+                "",
+                "width=600,height=400,left=200,top=200"
+            );
+            if (externalWindowRef.current) {
+                externalWindowRef.current.document.body.style.margin = "0px";
+                externalWindowRef.current.document.body.style.overflow =
+                    "hidden";
+
+                externalWindowRef.current.document.body.appendChild(
+                    containerRef.current
+                );
+                afterHook && afterHook(externalWindowRef.current);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (onMainBeforeUnload) {
+            window.addEventListener("beforeunload", onMainBeforeUnload);
+            return () => {
+                window.removeEventListener("beforeunload", onMainBeforeUnload);
+            };
+        }
+    }, [onMainBeforeUnload]);
+
+    useEffect(() => {
+        const externalWindow = externalWindowRef.current;
+        if (onExtBeforeUnload) {
+            externalWindowRef.current?.addEventListener(
+                "beforeunload",
+                onExtBeforeUnload
+            );
+            return () => {
+                externalWindow?.removeEventListener(
+                    "beforeunload",
+                    onExtBeforeUnload
+                );
+            };
+        }
+    }, [onExtBeforeUnload]);
+
+    return createPortal(children, containerRef.current!);
+});
+
+export default PortalWindow;
