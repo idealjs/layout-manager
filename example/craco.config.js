5cbe49844c4c6bdbede64ef53f9f283a57bb3eba
cqh963852 <cqh963852@hotmail.com>
2022-04-06 23:22:45 +0800

    update: update craco config

--- example/craco.config.js
+++ example/craco.config.js
@@ -1,10 +1,15 @@
+const path = require("path");
+
 module.exports = {
     webpack: {
         configure: (webpackConfig, { paths }) => {
             const tsLoader = {
                 test: /\.(js|mjs|jsx|ts|tsx)$/,
                 loader: require.resolve("ts-loader"),
-                options: { transpileOnly: true },
+                options: {
+                    transpileOnly: true,
+                    configFile: path.resolve(__dirname, "tsconfig.json"),
+                },
             };
             webpackConfig.module.rules.push(tsLoader);
             return webpackConfig;
