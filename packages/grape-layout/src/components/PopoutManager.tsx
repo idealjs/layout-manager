f13a9bd33b842a9c2e27d8d3664f941bacbe5530
cqh963852 <cqh963852@hotmail.com>
2022-04-08 12:18:57 +0800

    chore: remove useless log

--- packages/grape-layout/src/components/PopoutManager.tsx
+++ packages/grape-layout/src/components/PopoutManager.tsx
@@ -19,7 +19,6 @@ const PortalsContext = createContext<ContextType>(null);
 
 const PopoutManager = () => {
     const { portals } = usePortals();
-    console.log("test test", JSON.stringify(portals));
     return (
         <Fragment>
             {portals.map((d) => {
