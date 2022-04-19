b290549eae5d3877d0de628a1a55843a86f44572
cqh963852 <cqh963852@hotmail.com>
2022-04-20 00:17:34 +0800

    update: update export

--- packages/grape-layout/index.ts
+++ packages/grape-layout/index.ts
@@ -1,1 +1,6 @@
 export { default } from "./src/components/GrapeLayout";
+export * from "@idealjs/drag-drop";
+export * from "@idealjs/entity";
+export * from "@idealjs/layout-manager";
+export { default as PortalWindow } from "@idealjs/portal-window";
+export * from "@idealjs/sns";
