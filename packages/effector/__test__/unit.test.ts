3ab908e82b62d69dc0bc804ec398e7a476df9403
cqh963852 <cqh963852@hotmail.com>
2022-04-30 15:37:44 +0800

    chore: rename test file

--- packages/effector/__test__/unit.test.ts
+++ packages/effector/__test__/unit.test.ts
@@ -1,6 +1,6 @@
 import createUnit, { updateSymbol } from "../src/creator/createUnit";
 
-describe("event test", () => {
+describe("unit test", () => {
     test("should be triggered", (done) => {
         const $unit = createUnit<[number], number>((p) => p);
         const listener = jest.fn((state: number) => {
