c28180e47586facaa052efe218d919a7b0af10dc
cqh963852 <cqh963852@hotmail.com>
2022-05-12 12:55:55 +0800

    feat: add limit type to createEvent

--- packages/effector/src/creator/createEvent.ts
+++ packages/effector/src/creator/createEvent.ts
@@ -1,9 +1,15 @@
 import CommonEvent from "../classes/CommonEvent";
-import { IUnitOptions } from "../classes/CommonUnit";
+import CommonUnit, { IUnitOptions } from "../classes/CommonUnit";
+import { Effect } from "./createUnit";
+
+export interface IEvent<OutDone>
+    extends Omit<CommonUnit<OutDone[], OutDone, never>, "on" | "off"> {
+    (...params: Parameters<Effect<OutDone[], OutDone, never>>): Promise<void>;
+}
 
 const createEvent = <Payload = void>(
     unitOptions?: Omit<IUnitOptions, "type">
-) => {
+): IEvent<Payload> => {
     const commonEvent = new CommonEvent<Payload>(unitOptions);
 
     return Object.assign(commonEvent.runUnit, commonEvent);
