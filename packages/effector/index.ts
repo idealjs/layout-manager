ef68d284fe234548b400ff2a73d7a8b7d55b7cc7
cqh963852 <cqh963852@hotmail.com>
2022-05-15 12:24:26 +0800

    update: add export

--- packages/effector/index.ts
+++ packages/effector/index.ts
@@ -4,3 +4,4 @@ export { default as createStore } from "./src/creator/createStore";
 export { default as ScopeProvider } from "./src/react/ScopeProvider";
 export { default as useEvent } from "./src/react/useEvent";
 export { default as useStore } from "./src/react/useStore";
+export { default as useStoreMap } from "./src/react/useStoreMap";
