0d8095f500171b55dec151fb08470e70cf49b461
cqh963852 <cqh963852@hotmail.com>
2022-09-21 22:03:44 +0800

    refactor: split hook

--- packages/drag-drop/src/isHTMLElement.ts
+++ packages/drag-drop/src/isHTMLElement.ts
@@ -2,7 +2,8 @@ function isHTMLElement(
     el: Element,
     window: Window & typeof globalThis
 ): el is HTMLElement {
-    return el instanceof window.HTMLElement;
+    // return el instanceof window.HTMLElement;
+    return true;
 }
 
 export default isHTMLElement;
--- packages/grape-layout/src/components/Layout.tsx
+++ packages/grape-layout/src/components/Layout.tsx
@@ -26,11 +26,52 @@ import Titlebar from "./Titlebar";
 
 const Layout = () => {
     const ref = useRef<HTMLDivElement>(null);
-    const [rect] = useRect(ref);
-    const layoutNode = useLayoutNode();
+
     const splitters = useSplitters();
     const layouts = useLayouts();
     const panels = usePanels();
+
+    useInitSlotListener(ref);
+
+    return (
+        <div
+            ref={ref}
+            style={{ height: "100%", width: "100%", position: "relative" }}
+        >
+            {panels.map((p) => {
+                return <Panel key={p.id} nodeId={p.id} />;
+            })}
+            {layouts
+                .filter((l) => l.direction === LAYOUT_DIRECTION.TAB)
+                .map((n) => {
+                    return (
+                        <Titlebar key={n.id} nodeId={n.id}>
+                            {n.children.map((id) => (
+                                <Tab key={id} nodeId={id} />
+                            ))}
+                        </Titlebar>
+                    );
+                })}
+            {splitters.map((n) => {
+                return (
+                    <Splitter
+                        id={n.id}
+                        key={n.id}
+                        parentId={n.parentId}
+                        primaryId={n.primaryId}
+                        secondaryId={n.secondaryId}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default Layout;
+
+const useInitSlotListener = (ref: React.RefObject<HTMLDivElement>) => {
+    const [rect] = useRect(ref);
+    const layoutNode = useLayoutNode();
     const layoutSymbol = useLayoutSymbol();
     const sns = useSns();
     const slot = useSlot(layoutSymbol);
@@ -107,39 +148,4 @@ const Layout = () => {
         sns,
         update,
     ]);
-
-    return (
-        <div
-            ref={ref}
-            style={{ height: "100%", width: "100%", position: "relative" }}
-        >
-            {panels.map((p) => {
-                return <Panel key={p.id} nodeId={p.id} />;
-            })}
-            {layouts
-                .filter((l) => l.direction === LAYOUT_DIRECTION.TAB)
-                .map((n) => {
-                    return (
-                        <Titlebar key={n.id} nodeId={n.id}>
-                            {n.children.map((id) => (
-                                <Tab key={id} nodeId={id} />
-                            ))}
-                        </Titlebar>
-                    );
-                })}
-            {splitters.map((n) => {
-                return (
-                    <Splitter
-                        id={n.id}
-                        key={n.id}
-                        parentId={n.parentId}
-                        primaryId={n.primaryId}
-                        secondaryId={n.secondaryId}
-                    />
-                );
-            })}
-        </div>
-    );
 };
-
-export default Layout;
