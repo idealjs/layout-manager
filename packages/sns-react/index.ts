b6f42b3c6113f95b5eb73994711bba2ca011a3db
cqh963852 <cqh963852@hotmail.com>
2022-09-25 15:15:19 +0800

    chore: update package

--- packages/sns-react/index.ts
+++ packages/sns-react/index.ts
@@ -2,3 +2,4 @@ export { default as snsContext } from "./src/snsContext";
 export * from "./src/snsContext";
 export { default as useSlot } from "./src/useSlot";
 export { default as useSns } from "./src/useSns";
+export * from "@idealjs/sns-core";
