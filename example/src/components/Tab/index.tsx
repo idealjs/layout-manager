25042fe6027356e9107139f16090ff9a79095f5b
cqh963852 <cqh963852@hotmail.com>
2022-10-07 10:15:57 +0800

    refactor: split usePopout

--- example/src/components/Tab/index.tsx
+++ example/src/components/Tab/index.tsx
@@ -1,28 +1,18 @@
-import { IPoint } from "@idealjs/dnd-react";
 import {
-    useLayoutNode,
     useLayoutSymbol,
-    useMainLayoutSymbol,
     usePanel,
-    usePortals,
+    usePopout,
     useTabRef,
 } from "@idealjs/grape-layout";
 import {
-    ADD_PANEL_DATA,
-    LayoutNodeActionType,
-    MASK_PART,
-    PanelNode,
     REMOVE_PANEL_DATA,
-    ROOTID,
     SELECT_TAB_DATA,
     SLOT_EVENT,
     TABCMPT,
 } from "@idealjs/layout-manager";
-import { useSlot, useSns } from "@idealjs/sns-react";
+import { useSns } from "@idealjs/sns-react";
 import clsx from "clsx";
-import { nanoid } from "nanoid";
 import { useCallback } from "react";
-import { useMemo } from "react";
 
 import Close from "../svg/Close";
 import Popin from "../svg/Popin";
@@ -31,84 +21,12 @@ import styles from "./index.module.css";
 
 const Tab: TABCMPT = (props) => {
     const { nodeId } = props;
-    const { portals, setPortals } = usePortals();
 
-    const layoutNode = useLayoutNode();
     const layoutSymbol = useLayoutSymbol();
-    const mainLayoutSymbol = useMainLayoutSymbol();
     const sns = useSns();
-    const slot = useSlot(nodeId);
     const panel = usePanel(nodeId);
     const selected = panel?.selected;
-
-    const inPopout = useMemo(
-        () => portals.map((p) => p.id).includes(layoutSymbol),
-        [layoutSymbol, portals]
-    );
-
-    const popoutReady = useCallback(
-        (data: { layoutSymbol: string | number }) => {
-            console.debug("[Debug] popout ready", data);
-            const panelNode = new PanelNode({
-                panelJSON: panel!,
-            });
-            slot?.removeListener("ready", popoutReady);
-            sns.send(layoutSymbol, SLOT_EVENT.REMOVE_PANEL, {
-                search: panelNode.id,
-                mask: MASK_PART.CENTER,
-                target: ROOTID,
-            } as REMOVE_PANEL_DATA);
-
-            sns.send(data.layoutSymbol, SLOT_EVENT.ADD_PANEL, {
-                panelNode: panelNode,
-                mask: MASK_PART.CENTER,
-                target: ROOTID,
-            } as ADD_PANEL_DATA);
-        },
-        [layoutSymbol, panel, slot, sns]
-    );
-
-    const popout = useCallback(
-        (screen?: IPoint) => {
-            console.debug("[Debug] inPopout", inPopout);
-
-            if (inPopout) {
-                console.debug("[Debug] popin", mainLayoutSymbol);
-                const panelNode = new PanelNode({
-                    panelJSON: panel!,
-                });
-
-                layoutNode.doAction(LayoutNodeActionType.REMOVE_PANEL, {
-                    search: nodeId,
-                });
-
-                sns.send(mainLayoutSymbol, "popin", {
-                    panelNode: panelNode,
-                });
-            } else {
-                console.debug("[Debug] popout");
-                slot?.addListener("ready", popoutReady);
-                setPortals((s) => {
-                    return [
-                        ...s,
-                        { id: nanoid(), left: screen?.x, top: screen?.y },
-                    ];
-                });
-            }
-        },
-        [
-            inPopout,
-            panel,
-            layoutNode,
-            nodeId,
-            sns,
-            mainLayoutSymbol,
-            setPortals,
-            slot,
-            popoutReady,
-        ]
-    );
-
+    const [inPopout, popout] = usePopout(nodeId);
     const ref = useTabRef(nodeId, popout);
 
     const onSelect = useCallback(() => {
--- packages/grape-layout/src/hooks/index.ts
+++ packages/grape-layout/src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { default as useInitSlotListener } from "./useInitSlotListener";
 export { default as useMountedRef } from "./useMountedRef";
 export { default as usePanelRef } from "./usePanelRef";
+export { default as usePopout } from "./usePopout";
 export { default as useRect } from "./useRect";
 export { default as useSplitterRef } from "./useSplitterRef";
 export { default as useStateRef } from "./useStateRef";
