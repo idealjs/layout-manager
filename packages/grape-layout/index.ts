3372e1583806cc6a38f46634a67457fbaeb52521
cqh963852 <cqh963852@hotmail.com>
2023-11-12 23:26:32 +0800

    refactor: remove grape-layout portal-window

--- packages/grape-layout/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/features";
-export * from "./src/hooks";
-export * from "@idealjs/dnd-react";
-export * from "@idealjs/layout-manager";
-export { default as PortalWindow } from "@idealjs/portal-window";
--- packages/grape-layout/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as useInitSlotListener } from "./useInitSlotListener";
-export { default as useMountedRef } from "./useMountedRef";
-export { default as usePanelRef } from "./usePanelRef";
-export { default as usePopout } from "./usePopout";
-export { default as useRect } from "./useRect";
-export { default as useSplitterRef } from "./useSplitterRef";
-export { default as useStateRef } from "./useStateRef";
-export { default as useTabRef } from "./useTabRef";
-export { default as useUpdate } from "./useUpdate";
