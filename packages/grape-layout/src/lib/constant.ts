6705df533385a0aa48ef327e7f037f0d0748a6cc
cqh963852 <cqh963852@hotmail.com>
2023-11-12 23:26:32 +0800

    refactor: remove grape-layout portal-window

--- packages/grape-layout/src/lib/constant.ts
+++
@@ -1,8 +0,0 @@
-import { MASK_PART } from "@idealjs/layout-manager";
-
-export const rules = [
-    { part: MASK_PART.BOTTOM, max: 2 },
-    { part: MASK_PART.RIGHT, max: 2 },
-    { part: MASK_PART.TOP, max: 3, limitLevel: 1 },
-    { part: MASK_PART.CENTER, max: 2 },
-];
