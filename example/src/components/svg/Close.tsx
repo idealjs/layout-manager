c78e99821238a114c30967967080642753987070
cqh963852 <cqh963852@hotmail.com>
2023-11-12 23:27:03 +0800

    refactor: remove example website

--- example/src/components/svg/Close.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactComponent } from "./resources/close.svg";
-
-const Close = () => {
-    return <ReactComponent style={{ height: "100%", width: "100%" }} />;
-};
-
-export default Close;
--- example/src/components/svg/Popout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactComponent } from "./resources/popout.svg";
-
-const Popout = () => {
-    return <ReactComponent style={{ width: "100%", height: "100%" }} />;
-};
-
-export default Popout;
