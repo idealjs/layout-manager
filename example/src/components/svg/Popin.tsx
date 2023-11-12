4ed882d11ec4e02b26a1fe82400b66fc4b1f5e79
cqh963852 <cqh963852@hotmail.com>
2023-11-12 23:27:03 +0800

    refactor: remove example website

--- example/src/components/svg/Popin.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactComponent } from "./resources/popin.svg";
-
-const Popin = () => {
-    return <ReactComponent style={{ width: "100%", height: "100%" }} />;
-};
-
-export default Popin;
--- example/src/react-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react-scripts" />
-
-/// <reference types="@idealjs/grape-layout/custom"/>
